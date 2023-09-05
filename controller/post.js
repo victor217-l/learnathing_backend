fb9cb8f5a9c38ed98af49669f1d7d297868d302f
victor <victoribeojo8@gmail.com>
2023-09-06 00:56:14 +0100

    this is

--- controller/post.js
+++ controller/post.js
@@ -106,7 +106,7 @@ const upload = multer({ storage: storage });
 
 
 
-router.post('/addpost',  authenticateToken, upload.single("file"), async (req,res) => {
+router.post('/addpost',  authenticateToken, upload.single("images"), async (req,res) => {
 
   var title = req.body.title;
   //var filename = req.file.filename;
