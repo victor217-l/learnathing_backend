bd1ee0493e1826a02faafe608f343941c87b9e3b
victor <victoribeojo8@gmail.com>
2023-09-09 00:09:25 +0100

    this is

--- controller/post.js
+++ controller/post.js
@@ -64,7 +64,7 @@ router.use(bodyparser.json())
 
 
 const admin = require("firebase-admin");
-const serviceAccount = require("../google-services .json");
+const serviceAccount = require("../google-services.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
